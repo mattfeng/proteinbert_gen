051508a34c57e320aa81f4bd292896ca1b68fb6c
Matthew Feng <mattfeng@mit.edu>
2024-05-04 12:54:37 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='proteinbert_gen',
+    version='0.1.0',
+    description='',
+    author_email=''
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[]
+)
